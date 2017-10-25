18e6a1dd3bcb88282cfb80189ccc08aa067561ec
pfernandezalonso <pfernandezalonso@victoria29.danielcastelao.org>
2017-10-25 09:38:00 +0200

    Boletin2_1 Terminado

---
+++ src/boletin2_1/Boletin2_1.java
@@ -0,0 +1,18 @@
+
+package boletin2_1;
+
+
+
+
+public class Boletin2_1 {
+
+    
+    public static void main(String[] args) {
+        
+    float base=4F, altura=3F, area;
+    area = base*altura/2;
+    System.out.println("el area del triangulo es = "+area);
+    
+    }
+    
+}
